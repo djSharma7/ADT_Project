70e353bf49fcd9960326ee0aef7c1565f7b6f257
Deepanshu Sharma <32371604+djSharma7@users.noreply.github.com>
2022-08-26 14:38:47 -0400

    Output Stream Writing File

--- OutputWriter.py
+++ OutputWriter.py
@@ -16,6 +16,8 @@ class OutputWriter:
         i=-1
         try:
             data = self.db_obj.get_historical_keyword_data()
+            # with open('output.json','r') as d:
+            #     data= json.load(d)
             try:
                 with open('Results/JSON/output.json','w') as d:
                     json.dump(data,d)
@@ -34,45 +36,44 @@ class OutputWriter:
                         all_format = workbook.add_format({
                             'valign': 'vcenter',
                             'align': 'center'})
-                        self_sku_format = workbook.add_format({'valign': 'vcenter',
-                                                               'align': 'center',
-                                                               'font_color': '#b8e874'})
-                        comp_sku_format = workbook.add_format({'valign': 'vcenter',
-                                                               'align': 'center',
-                                                               'font_color': '#eb6154'})
 
-                        for date_ in keyword_data_obj:
-                            date_obj = keyword_data_obj.get(date_, [])
-                            df = pd.read_json(json.dumps(date_obj))
-                            self_rows = np.where(df['SKU Type'].isin(['Self']),
-                                                 'background-color: #94d964',
-                                                 '')
-                            comp_rows = np.where(df['SKU Type'].isin(['Competitor']),
-                                                 'background-color: #ed645c',
-                                                 '')
-                            # Apply calculated styles to each column:
-                            styler = df.style.apply(lambda _: self_rows)
-                            # styler = df.style.apply(lambda _: comp_rows)
-                            styler.to_excel(writer, sheet_name=date_, index=False, startrow=1, header=False)
+                        for date_o in keyword_data_obj:
+                            for date_ in date_o:
+                                date_obj = date_o.get(date_, [])
+                                df = pd.read_json(json.dumps(date_obj))
+                                self_rows = np.where(df['SKU Type'].isin(['Self']),
+                                                     'background-color: #94d964',
+                                                     '')
+                                comp_rows = np.where(df['SKU Type'].isin(['Competitor']),
+                                                     'background-color: #ed645c',
+                                                     '')
+                                # Apply calculated styles to each column:
+                                styler = df.style.apply(lambda _: self_rows)
+                                # styler = df.style.apply(lambda _: comp_rows)
+                                styler.to_excel(writer, sheet_name=date_, index=False, startrow=1, header=False)
 
-                            worksheet = writer.sheets[date_]
-                            worksheet.set_column('A:K', None, all_format)
+                                worksheet = writer.sheets[date_]
+                                worksheet.set_column('A:K', None, all_format)
 
-                            header_color = self.header_colors[i]
-                            header_format = workbook.add_format({
-                                'bold': True,
-                                'text_wrap': True,
-                                'valign': 'vcenter',
-                                'align': 'center',
-                                'bg_color': header_color,
-                                'border': 1})
-                            for col_num, value in enumerate(df.columns.values):
-                                worksheet.write(0, col_num, value, header_format)
-                            worksheet.set_column('A:A', 10)
-                            worksheet.set_column('B:B', 20)
-                            worksheet.set_column('C:D', 30)
-                            worksheet.set_column('E:E', 20)
-                            worksheet.set_column('F:F', 30)
-                            worksheet.set_column('G:K', 20)
+                                header_color = self.header_colors[i]
+                                header_format = workbook.add_format({
+                                    'bold': True,
+                                    'text_wrap': True,
+                                    'valign': 'vcenter',
+                                    'align': 'center',
+                                    'bg_color': header_color,
+                                    'border': 1})
+                                for col_num, value in enumerate(df.columns.values):
+                                    worksheet.write(0, col_num, value, header_format)
+                                worksheet.set_column('A:A', 10)
+                                worksheet.set_column('B:B', 20)
+                                worksheet.set_column('C:D', 30)
+                                worksheet.set_column('E:E', 20)
+                                worksheet.set_column('F:F', 30)
+                                worksheet.set_column('G:K', 20)
         except Exception as e:
             print ("Exception in output writer :-",e)
+
+
+if __name__ == '__main__':
+    obj = OutputWriter(None)
